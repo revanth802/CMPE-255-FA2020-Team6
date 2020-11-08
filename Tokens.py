d880998775c9dec2c435fab1e0b1cdc6160d76ed
revanth802 <revanthkrishna.maddula@sjsu.edu>
2020-11-07 22:05:40 -0800

    tweets fetched using Tweepy

---
+++ Tokens.py
@@ -0,0 +1,12 @@
+# Access Token
+access_token='147456806-g7iCcim6wCUUJxGicH88PBY26QzQ3a1txMdgsyXC'
+
+# Access Token Secret
+access_secret='1oy2XDVQlKRlrESzmkDFpBXjcZiSqKdcDBE39z3NYqiau'
+
+
+# API key:
+consumer_key= 'Pd6TsxPmCVPiM2dIauNoZE5j2'
+
+# API secret key:
+consumer_secret= '7SubRF8AfHT2IRxEVMy3b3Y6p1wTDgTutCG5VMpvcYkpeMJIr6'
